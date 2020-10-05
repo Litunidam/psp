e41d7493be9ebf56ac82d248c70c4eb665f314c2
Manuel Martín Benito <manuelmarb@gmail.com>
2020-10-05 18:17:50 +0200

    exercises

--- src/primerosEjercicios/Ejercicio1.java
+++ src/primerosEjercicios/Ejercicio1.java
@@ -1,5 +1,15 @@
 package primerosEjercicios;
 
 public class Ejercicio1 {
-
+	
+	public static void main(String[] args) {
+		
+		for (int i=0;i<6;i++) {
+			for (int j=0;j<i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	
+	}
 }
